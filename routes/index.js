6b0a29eebd874cd2b23ed1f01ecf01133cec5bb9
Brian Antunes <brian.antunes@yahoo.com>
2022-08-27 20:17:19 -0400

    Refactored index.js routes

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,16 @@ var apiModel = require('../models/apiModel');
 // Create API object
 let apodURL = {}
 
-// Perform API Promise call, assign values to variable apodUrl
+// Perform API Promise call, assign object to apodUrl
 apiModel().then((api) =>{
-  apodURL.url = api['url']['url']
-  apodURL.exp = api['url']['explanation']
-  apodURL.title = api['url']['title']
-  apodURL.media_type = api['url']['media_type']
+  apodURL = api.url
 }).catch(console.error)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-  ];
-
-  var tagline = "No programming concept is complete without a cute animal mascot.";
 
   // pass apodUrl and perform check on view if media_type is equal to image
-  res.render('index', { mascots: mascots, tagline: tagline, apod: apodURL });
+  res.render('index', { apod: apodURL });
 });
 
 module.exports = router;
